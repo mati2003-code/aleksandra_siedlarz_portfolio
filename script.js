62aec7dfdea4d2fa2d4c2b17d58260b367c7dc57
mati2003-code <mateusz.dziobakop.pl@op.pl>
2024-08-12 12:08:58 +0200

    sections up to date

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-// Skrypt lightbox
-document.addEventListener("DOMContentLoaded", function() {
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImg = document.getElementById("lightbox-img");
-    const galleryItems = document.querySelectorAll(".gallery-item");
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    const closeBtn = document.querySelector('.close');
 
     galleryItems.forEach(item => {
-        item.addEventListener("click", function() {
-            lightbox.style.display = "flex";
+        item.addEventListener('click', function () {
+            lightbox.style.display = 'flex';
             lightboxImg.src = this.src;
         });
     });
 
-    const closeLightbox = document.querySelector(".lightbox .close");
-    closeLightbox.addEventListener("click", function() {
-        lightbox.style.display = "none";
+    closeBtn.addEventListener('click', function () {
+        lightbox.style.display = 'none';
+    });
+
+    lightbox.addEventListener('click', function (e) {
+        if (e.target !== lightboxImg && e.target !== closeBtn) {
+            lightbox.style.display = 'none';
+        }
+    });
+
+    // Zamknięcie lightboxa przy naciśnięciu klawisza Escape
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape') {
+            lightbox.style.display = 'none';
+        }
     });
 });
+
+
 
